0975080d0fc85e2ed514fe97a3ca09e63af4af5f
c87 <shaun.carmody@live.co.uk>
2019-03-19 21:05:43 +0000

    remove root get request

--- server.js
+++ server.js
@@ -5,13 +5,6 @@ const postsRouter = require('./posts/postsRouter.js');
 const server = express();
 server.use(express.json());
 
-server.get('/', (req, res) => {
-  res.send(`
-    <h2>Lambda Posts API</h>
-    <p>Welcome to the Lambda Posts API</p>
-  `);
-});
-
 server.use('/api/posts', postsRouter);
 
 module.exports = server;
